2f6a20238391bacb81a26c31aeaa0d85d509e232
Miguel Lamarca Moreira <50866670890@info.local>
2023-03-29 17:17:51 -0300

    first commit

---
+++ projeto 1/Program.cs
@@ -0,0 +1,87 @@
+﻿// variaveis**
+
+// declarando variavel
+// tipo nome = valor
+// string nome = "leugim";
+// nome = " cleiton ";
+// const int idade = 69;
+// Console.WriteLine($"a idade do" + nome + "é " + idade + " anos " + "de cueca no quarto");
+
+// tipos de dados**
+// int quantidade = 10;
+// double preco = 4.99D;
+// float altura = 1.80f;
+// bool careca = true;
+// string texto = "olá, mundo!";
+// char letra = 'C';
+// Console.WriteLine("nao sei");
+
+// operadores aritiméticos**
+
+// soma
+// Console.WriteLine(4 + 4);
+// Console.WriteLine(4 + "4");
+
+// subtração
+// Console.WriteLine(4 - 2);
+
+// divisão
+// Console.WriteLine(4 / 2);
+
+// multiplicação
+// Console.WriteLine(4 * 2);
+
+// modular
+// Console.WriteLine(5 % 2);
+
+// operadores de compraração**
+
+// igual a 
+// Console.WriteLine(5 == 6);
+
+// maior ou igual a 
+// Console.WriteLine(5 >= 4);
+
+// menor ou igual a 
+// Console.WriteLine(4 <= 4);
+
+//maior que 
+// Console.WriteLine(5 > 1);
+
+//menor que 
+// Console.WriteLine(5 < 8);
+
+// diferente de 
+// Console.WriteLine(5 != 8);
+
+// operadores logicos**
+
+// && : e
+// || : ou
+//  ! : negação
+
+// Console.WriteLine(5 == 5 && 8 == 8); //true && true = true
+// Console.WriteLine(5 == 5 && 8 == 9); //true && false = false
+// Console.WriteLine(5 == 6 && 8 == 8); //false && true = false
+
+
+// Console.WriteLine(5 == 5 || 8 == 8); //true || true = true
+// Console.WriteLine(5 == 5 || 8 == 9); //true || false = true
+// Console.WriteLine(5 == 6 || 8 == 8); //false || true = true
+// Console.WriteLine(5 == 6 || 8 == 8); //false || true = false
+
+
+// Console.WriteLine(!(5 == 5 || 8 == 9)); //true || false = fals
+// Console.WriteLine(!(5 == 6 || 8 == 8)); //false || true = false
+// Console.WriteLine(!(5 == 6 || 8 == 8)); //false || true = true
+
+//  programa criado para calcular imc
+
+// string nomes = "Miguel";
+// float peso = 90.8f;
+// float alturas = 1.90f;
+
+// porcessamento
+// float imc = peso / (alturas * alturas);
+
+// Console.WriteLine($"o IMC do " + nomes + " é de : " + Math.Round(imc,2)); 
