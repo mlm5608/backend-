92ae93fba0ed7c85877e4eea0704de42e4762983
Miguel Moreira <miguelmoreira5608@gmail.com>
2023-04-02 16:43:22 -0300

    atualização

---
+++ projetos lista 2/projeto indentificador numeral/Program.cs
@@ -0,0 +1,13 @@
+﻿// Faça um programa que leia três números e mostre o maior e o menor deles.
+
+Console.WriteLine("descobrir qual número é maior e qual é menor");
+
+Console.WriteLine($"digite o 1º valor");
+int valor1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"digite o 2º valor");
+int valor2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"digite o 3º valor");
+int valor3 = int.Parse(Console.ReadLine());
+
