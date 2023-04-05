6bb7711a5c9e77c37ea9dc326673e4364418f901
Miguel Lamarca Moreira <50866670890@info.local>
2023-04-05 17:22:45 -0300

    atualização

--- exercicios/exercicio laços de repeticão/Program.cs
+++ exercicios/exercicio laços de repeticão/Program.cs
@@ -7,53 +7,59 @@
 // resolução
 
 // =================================================================
-Console.WriteLine($"digite seu nome:");
-string nome = Console.ReadLine();
+// Console.WriteLine($"digite seu nome:");
+// string nome = Console.ReadLine();
 
-while (nome == "")
+// while (nome == "")
+// {
+//    Console.WriteLine($"Digite um nome válido!");
+//    nome = Console.ReadLine();
+// }
+
+// // =================================================================
+// Console.WriteLine($"digite sua idade:");
+// int idade = int.Parse(Console.ReadLine());
+
+// while ((idade < 0) || (idade > 100))
+// {
+//     Console.WriteLine($"digite uma idade válida!");
+//     idade = int.Parse(Console.ReadLine());
+// }
+
+// // =================================================================
+// Console.WriteLine($"digite seu salário:");
+// float salario = float.Parse(Console.ReadLine());
+
+// while (salario <= 0)
+// {
+//     Console.WriteLine($"digite um salário maior que zero!");
+//     salario = float.Parse(Console.ReadLine());
+// }
+// // =================================================================
+// Console.WriteLine(@$"
+// digite uma dessas opções:
+// s = solteiro(a)
+// c = casado(a)
+// v = viúvo(a)
+// d = divorciado(a)
+// ");
+// char estCivil = char.Parse(Console.ReadLine().ToUpper());
+
+
+// while ((estCivil != 'S') && (estCivil != 'C') && (estCivil != 'V') && (estCivil != 'D'))
+// {
+//     Console.WriteLine($"digite um estado civil válido");
+//     Console.WriteLine(@$"
+//     s = solteiro(a)
+//     c = casado(a)
+//     v = viúvo(a)
+//     d = divorciado(a)
+//     ");
+//     estCivil = char.Parse(Console.ReadLine().ToUpper());
+// }
+
+for (int x = 500; x >= 0; x-=5)
 {
-   Console.WriteLine($"Digite um nome válido!");
-   nome = Console.ReadLine();
-}
-
-// =================================================================
-Console.WriteLine($"digite sua idade:");
-int idade = int.Parse(Console.ReadLine());
-
-while ((idade < 0) || (idade > 100))
-{
-    Console.WriteLine($"digite uma idade válida!");
-    idade = int.Parse(Console.ReadLine());
-}
-
-// =================================================================
-Console.WriteLine($"digite seu salário:");
-float salario = float.Parse(Console.ReadLine());
-
-while (salario <= 0)
-{
-    Console.WriteLine($"digite um salário maior que zero!");
-    salario = float.Parse(Console.ReadLine());
-}
-// =================================================================
-Console.WriteLine(@$"
-digite uma dessas opções:
-s = solteiro(a)
-c = casado(a)
-v = viúvo(a)
-d = divorciado(a)
-");
-char estCivil = char.Parse(Console.ReadLine().ToUpper());
-
-
-while ((estCivil != 'S') && (estCivil != 'C') && (estCivil != 'V') && (estCivil != 'D'))
-{
-    Console.WriteLine($"digite um estado civil válido");
-    Console.WriteLine(@$"
-    s = solteiro(a)
-    c = casado(a)
-    v = viúvo(a)
-    d = divorciado(a)
-    ");
-    estCivil = char.Parse(Console.ReadLine().ToUpper());
+    Console.WriteLine(x);
+    
 }
