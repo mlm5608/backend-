6aca76df82942ae610d945885b9baaaec05ecfa0
Miguel Lamarca Moreira <50866670890@info.local>
2023-04-06 17:02:51 -0300

    aula dia 06/04

---
+++ exercicios/projetos lista 3 (dia 6 de abril)/exercicio 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+Console.WriteLine($"digite um número para ver sua tabuada até 10");
+int numero = int.Parse(Console.ReadLine());
+
+int multiplicador = 1;
+
+while ( multiplicador <= 10)
+{
+    int resultado = numero * multiplicador;
+    Console.WriteLine(@$" {multiplicador} X {numero} = {resultado}");
+
+    multiplicador++;
+}
+
+
