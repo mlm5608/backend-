71c8b5f2c1d2e93348398ef0388db6a226cf4cdf
Miguel Lamarca Moreira <50866670890@info.local>
2023-04-10 17:23:42 -0300

    aula dia 10/04

---
+++ exemplos/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿// sem array
+
+string carro1;
+string carro2;
+string carro3;
+
+Console.WriteLine($"digite o nome do carro:");
+carro1 = Console.ReadLine()!;
+Console.WriteLine($"digite o nome do carro:");
+carro2 = Console.ReadLine()!;
+Console.WriteLine($"digite o nome do carro:");
+carro3 = Console.ReadLine()!;
+
+Console.WriteLine($"{carro1} {carro2} {carro3}");
+
+// com array
+
+string[] carros = new string[3];
+
+Console.WriteLine($"digite o nome do carro");
+carros[0] = Console.ReadLine()!;
+
+Console.WriteLine($"digite o nome do carro");
+carros[1] = Console.ReadLine()!;
+
+Console.WriteLine($"digite o nome do carro");
+carros[2] = Console.ReadLine()!;
+
+Console.WriteLine($"o 1º carro da lista: {carros[0]}");
+Console.WriteLine($"o 2º carro da lista: {carros[1]}");
+Console.WriteLine($"o 3º carro da lista: {carros[2]}");
