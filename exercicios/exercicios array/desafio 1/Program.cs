82155649d3edbf3dea54c61718b3799b34a729b9
Miguel Lamarca Moreira <50866670890@info.local>
2023-04-11 17:23:44 -0300

    aula dia 11/4

---
+++ exercicios/exercicios array/desafio 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Faça um programa que carregue um array com 6 numeros inteiros. calcule e imprima a quantidade de números ímpares e a quantidade de números pares.
+
+int[] num = new int[6];
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine($"giti o {i+1}º numero");
+    num[i] = int.Parse(Console.ReadLine()!);
+}
