4ee8812a5c6d62fde5a59d1b88d5fcba211fe9de
Miguel Lamarca Moreira <50866670890@info.local>
2023-05-26 17:19:08 -0300

    aula dia 26/05

--- Back2/Exercícios/Evento_MVC/Model/Evento.cs
+++ Back2/Exercícios/Evento_MVC/Model/Evento.cs
@@ -10,5 +10,26 @@ namespace Evento_MVC.Model
         public string? Nome { get; set; }
         public string? Descrição { get; set; }
         public string? Data { get; set; }
+
+        private const string PATH = "Database/Evento.csv";
+
+        public Evento()
+        {
+        // criar lógica para gerar pasta e produto
+
+            // obter o caminho da pasta
+            string pasta = PATH.Split("/")[0];
+ 
+            // verificar se na pasta já esxiste uma pasta
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+            // verificar se na pasta já esxiste um arquivo
+            if (!File.Exists(PATH))
+            {
+                File.Create(PATH);
+            }
+        }
     }
 }
